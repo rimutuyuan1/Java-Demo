c78dd3a62c8d191df7d7f5bf72ededb14f9168fa
王鑫涛 <17600307262@163.com>
2020-04-10 16:04:23 +0800

    用ReentrantLock和atomic保证数据安全

--- thread/src/main/java/com/forezp/thread/threadPool/Atomic.java
+++ thread/src/main/java/com/forezp/thread/threadPool/Atomic.java
@@ -9,11 +9,16 @@ public class Atomic {
     private static AtomicInteger count = new AtomicInteger(0);
     private static ExecutorService executorService = Executors.newFixedThreadPool(10);
 
+    private static synchronized void incr() {  // 加上sync关键字 顺序打印  不加sync关键字  不会顺序打印
+        int i = count.incrementAndGet();
+        System.out.printf("count ++ : %s\n", i);
+    }
+
     private static void thread() {
         for (int request = 0; request < 1000; request++) {
-            executorService.execute(() -> count.incrementAndGet());
+            executorService.execute(Atomic::incr);
         }
-        System.out.printf("count ++ : %s\n", count.get());
+        System.out.println(count.get());
     }
 
     public static void main(String[] args) {
