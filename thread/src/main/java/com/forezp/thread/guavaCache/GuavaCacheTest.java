c58b2cddce69aef250b12117d4bf94880a18c676
王鑫涛 <17600307262@163.com>
2020-04-18 12:27:03 +0800

    add lombok note

--- thread/src/main/java/com/forezp/thread/guavaCache/GuavaCacheTest.java
+++ thread/src/main/java/com/forezp/thread/guavaCache/GuavaCacheTest.java
@@ -6,7 +6,9 @@ import com.google.common.cache.CacheBuilder;
 public class GuavaCacheTest {
 
     public static void main(String[] args) {
-        Cache<String,String> cache = CacheBuilder.newBuilder().build();
+        Cache<String,String> cache = CacheBuilder.newBuilder()
+                .maximumSize(2)
+                .build();
         cache.put("word","Hello Guava Cache");
         System.out.println(cache.getIfPresent("word"));
     }
