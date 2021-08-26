d60305fc31eaf307d9180d86f8850d75ca78c296
王鑫涛 <17600307262@163.com>
2021-08-26 23:22:14 +0800

    fix

---
+++ thread/src/main/java/com/forezp/thread/classlaoder/ParentClassLoaderTest.java
@@ -0,0 +1,13 @@
+package com.forezp.thread.classlaoder;
+
+/**
+ * @Author 王鑫涛
+ * @Description TODO
+ * @Date 2021/8/26 下午10:51
+ **/
+public class ParentClassLoaderTest {
+
+    public static void main(String[] args) {
+        System.out.println(sun.misc.Launcher.getLauncher().getClassLoader());
+    }
+}
